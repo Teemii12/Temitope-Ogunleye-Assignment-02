122450cd997597b2f319d2f428cd61dd347b65cc
Ogunleye Temitope <ogunleyetemitope77@gmail.com>
2022-07-16 20:57:15 -0700

    assignment done

--- src/assignment.js
+++ src/assignment.js
@@ -33,9 +33,15 @@ assignment.sumOfNumbers = sumOfNumbers;
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let number = 0
+    for(let i = 0; i < arrayOfNumbers.length; i++){
+        if(arrayOfNumbers[i] % 2 === 0){
+            number++
+        }
+    }
+    return number
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
